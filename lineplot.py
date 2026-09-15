b321fa108239dfe2f4db79a2ae1c60953780070d
Anusha VK <anushavk851@gmail.com>
2026-09-15 12:15:06 +0530

    Update and rename linechart.py to lineplot.py

--- lineplot.py
+++ lineplot.py
@@ -2,3 +2,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 sns.lineplot(data=df,x="total_bill",y="tip")
 plt.show()
+
+df1=sns.load_dataset('flights')
+df1
+sns.lineplot(data=df1,x="year",y="passengers")
+plt.show()

a3536697bb81395a6f4de84d688e5c560e26a691
Anusha VK <anushavk851@gmail.com>
2026-09-15 12:07:02 +0530

    Implement day-wise total bill bar plots
    Added bar plots for total bill analysis by day using seaborn.

--- Bar plots.py
+++ Bar plots.py
@@ -14,3 +14,15 @@ sns.get_dataset_names()
 # loading dataset from seaborn we use load_dataset function
 import pandas as pd
 df=sns.load_dataset('tips')
+
+#day wise totalbill analysis
+print(df['day'].unique())
+sns.barplot(data=df,x="day",y="total_bill",errorbar=None)
+plt.show()
+#height of bar is the average of total bill(by default aggregation is mean)
+
+#total of totalbill in each category
+import numpy as np
+print(df['day'].unique())
+sns.barplot(data=df,x="day",y="total_bill",estimator=np.sum,errorbar=None)
+plt.show()

6e132dd031b68a3f013f2a1dd98283d68df69160
Anusha VK <anushavk851@gmail.com>
2026-09-15 12:12:23 +0530

    Add comment for pair plot explanation
    Added a comment explaining the purpose of the pair plot.

---
+++ Pair Plot.py
@@ -0,0 +1,6 @@
+#pair plot
+#show relationship between multiple numerical variables
+sns.pairplot(df)
+plt.show()
+
+#to plot all the combination of numerical column in the dataset without specifying each
